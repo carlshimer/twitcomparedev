482392102a7db9c07ea80e684b9da840a8621127
Carl Shimer <carl@exoprise.com>
2012-05-28 14:18:12 -0400

    iniital rails app generation with some gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitcomparedev::Application.config.secret_token = '8fb007c0331c4d1280872ba942b3e6fc60086171cebb44e8e31acc2c7a58a4f3d0a702a17defff32400135dfdf35d5bad09f5cde0883bf363ec41eadd14262e8'
---
+++ initializers/twitter_config.rb
@@ -0,0 +1,9 @@
+# TODO: don't check this in.
+
+
+Twitter.configure do |config|
+  config.consumer_key = "zTzFVD8KfSWVMoQ7LCj7g"
+  config.consumer_secret = "iDNTt8UhRcSfxk5DTxN3q0KIEPNQoPtsVXC28sCMA"
+  config.oauth_token = "45049019-lB9ga2KbwvmBsgfEgNiSifOMPWgukca9yWsaVXchD"
+  config.oauth_token_secret = "ANbIxXJNDFrmWTNkdREOt6gvBpkoGACuDwur6FYUDU"
+end
